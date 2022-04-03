366106d8622c6cdfacefa766d80f143cb0b9c371
Wayne M <waynemot@msn.com>
2022-04-03 18:57:53 -0500

    add generate route member

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   resources :doc_templates
   resources :employment_types
   resources :requests
-  resources :letters
+
+  resources :letters do
+    member do
+      get 'generate'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'requests#index'
 end
